575d8d1b734246ec3b5c909c5fb4a88e2066be46
Eric Park <ideamaneric@gmail.com>
2019-03-28 09:49:20 +0800

    URL filter

--- parser.py
+++ parser.py
@@ -1,6 +1,23 @@
 import urllib
 
-video_url_lib = urllib.FancyURLopener()
+'''
+Parses the URL to retreive the video.
 
+@returns:
+    0 - Successful
+    1 - Not link
+    2 - Unsupported link or text
+'''
 def parseUrl(link):
-    video_url_lib.retrieve(link)
+    if 'wetransfer' in link:
+        # WeTransfer link
+        print('Got WeTransfer link: {}'.format(link))
+    elif 'http' not in link:
+        # Possible we got a bad link
+        print('Bad link, not parsing: {}'.format(link))
+        return 1
+    else:
+        # Unsupported
+        print('Unsupported: {}'.format(link))
+        return 2
+    return 0
