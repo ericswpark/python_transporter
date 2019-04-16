07650c99029834bc6b9fa6f266e8692f93752c25
Eric Park <ideamaneric@gmail.com>
2019-04-16 08:56:33 +0800

    Fix "move transferwee.py to utils/ folder"
    Python can't find this file because we invoke it from main.py. Fix this by moving it into utils/

--- utils/parser.py
+++ utils/parser.py
@@ -10,8 +10,8 @@ def parseUrl(link) -> int:
     if 'wetransfer' in link or 'we.tl' in link:
         # WeTransfer link
         print('Got WeTransfer link: {}'.format(link))
-        import transferwee
-        transferwee.download(link)
+        import utils.transferwee
+        utils.transferwee.download(link)
     elif 'http' not in link:
         # Possible we got a bad link
         print('Bad link, not parsing: {}'.format(link))
